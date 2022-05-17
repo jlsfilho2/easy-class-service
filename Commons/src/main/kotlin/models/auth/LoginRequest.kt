4921a9ab87ea090e9351d9299005e9d4c08ce7dd
Matheus César <matheusilvacesar@gmail.com>
2022-05-17 01:06:31 -0300

    Login working

---
+++ Commons/src/main/kotlin/models/auth/LoginRequest.kt
@@ -0,0 +1,5 @@
+package models.auth
+
+data class LoginRequest(
+    var userRemoteId: String = ""
+)
---
+++ LoginService/src/main/kotlin/app/App.kt
@@ -0,0 +1,25 @@
+package app
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import dynamo.DynamoDBUtils
+import dynamo.user.RegisterUserRepository
+import models.auth.LoginRequest
+import requests.requestResponse
+import java.io.IOException
+
+class App : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    override fun handleRequest(input: APIGatewayProxyRequestEvent?, context: Context?): APIGatewayProxyResponseEvent {
+        val loginRequest = DynamoDBUtils.objectMapper.readValue(input?.body, LoginRequest::class.java)
+        val registerUserRepository = RegisterUserRepository()
+        val data = registerUserRepository.getUserByRemoteId(loginRequest.userRemoteId)
+
+        return try {
+            requestResponse(data = data, status = 200)
+        } catch (exception: IOException) {
+            requestResponse(data = null, status = 500)
+        }
+    }
+}
